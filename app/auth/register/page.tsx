dc6d10893f55c7f8c65be8febd32f8cd22b05a19
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2025-05-13 08:13:19 -0500

    se crea el login

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,11 +1,6 @@
-import {create} from 'zustand';
-interface AuthState{
-    isAuthenticated:boolean;
-    token:string|null;
-    setAuthenticated:(auth:boolean,token:string|null)=>void;
+import { RegisterForm } from "@/app/components/auth/RegisterForm";
+export default function RegisterPage() {
+  return (
+    <RegisterForm/>
+  );
 }
-export const useAuthStore=create<AuthState>((set)=>({
-    isAuthenticated:false,
-    token:null,
-    setAuthenticated:(auth,token)=>set({isAuthenticated:auth,token})
-}))
